41d4b4de16b6cbc9d2f7b48bb8e8ea64858e867a
Chidalu Nsobundu <kidstarro22@gmail.com>
2022-10-02 12:30:49 +0100

    added period

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 let homeScore= document.getElementById("hs")
 let guestScore= document.getElementById("gs")
+let periodOut= document.getElementById("p")
 let hScore = 0
 let gScore=0
+let per= 1
 
 
 function reset(){
     hScore=0
     gScore=0
+    per=1
     homeScore.textContent=hScore
     guestScore.textContent=gScore
+    periodOut.textContent=per
     guestScore.style.color="black"
     homeScore.style.color="black"
+}
+function period(){
+    if(per<=3){
+        per+=1
+        periodOut.textContent=per
+    }
 }
 function addOneHome(){addh("addOne")}
 function addTwoHome(){addh("addTwo")}
